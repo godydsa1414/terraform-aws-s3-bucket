647257f7d9f4360901567342db4cc75f2c0f3742
godydsa1414 <88794215+godydsa1414@users.noreply.github.com>
2021-08-11 14:25:18 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -43,7 +43,7 @@ variable "bucket" {
 variable "bucket_prefix" {
   description = "(Optional, Forces new resource) Creates a unique bucket name beginning with the specified prefix. Conflicts with bucket."
   type        = string
-  default     = null
+  default     = "This prefix will be included in the name of most resources"
 }
 
 variable "acl" {
